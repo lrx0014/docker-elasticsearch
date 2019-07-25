577ffeac591d3f7c1f5ec1f83299338877fead37
renxiang <renxiang.li@daocloud.io>
2019-07-25 17:17:59 +0800

    fix sgadmin

--- src/auth/sgadmin.sh
+++ src/auth/sgadmin.sh
@@ -4,7 +4,7 @@ sgadmin(){
     chmod +x /elasticsearch/plugins/search-guard-6/tools/sgadmin.sh
     /elasticsearch/plugins/search-guard-6/tools/sgadmin.sh \
     -cd /elasticsearch/config/searchguard \
-    -ks /elasticsearch/config/certs/es-route-keystore.jks \
+    -ks /elasticsearch/config/certs/sgadmin-keystore.jks \
     -ts /elasticsearch/config/certs/truststore.jks \
     -cn $CLUSTER_NAME \
     -kspass $KS_PWD \
